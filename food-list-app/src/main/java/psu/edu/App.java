4832649261e79c8182f607a3f27711485ec59b46
Daniel Frederick <dpfrederick@webstaurantstore.com>
2022-05-02 16:40:55 -0400

    blah

--- food-list-app/src/main/java/psu/edu/App.java
+++ food-list-app/src/main/java/psu/edu/App.java
@@ -41,7 +41,8 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        // launch();
+        console.log("Hello World!");
     }
 
 }
